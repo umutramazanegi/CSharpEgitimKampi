48146492e31bd1a9b41574d6143901d7ab8c1d0e
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2024-10-24 16:00:20 +0300

    SQL Veri Tabanı ve Tablo Oluşturma & C# Kısmına Bağlama

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace _09_DatabaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# Veri Tabanlı ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen Getirmek İstediğiniz Tablo Numarasını Giriniz:    ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("********************************************************");
+
+            SqlConnection connection = new SqlConnection("Data Source=UMUT\SQLEXPRESS;Initial Catalog=EgitimKampiDb;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select*from TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var Items in row.ItemArray)
+                {
+                    Console.Write(Items.ToString());
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.Read();
+
+        }
+    }
+}
